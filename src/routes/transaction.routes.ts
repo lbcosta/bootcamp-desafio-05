a7650b11a563bc27dd622a218ebdad5664e74ab1
Leonardo Costa <lbcosta.dev@gmail.com>
2020-04-22 00:21:51 -0300

    Passing all tests

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
@@ -7,9 +8,33 @@ const transactionRouter = Router();
 
 // const transactionsRepository = new TransactionsRepository();
 
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  type: string;
+}
+
+const transactions: Transaction[] = [];
+
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const income = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((inc, transaction) => inc + transaction.value, 0);
+
+    const outcome = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((out, transaction) => out + transaction.value, 0);
+
+    return response.json({
+      transactions,
+      balance: {
+        income,
+        outcome,
+        total: income - outcome,
+      },
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +42,30 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    /**
+     * type: Tipo da transação -> Income = Depósitos | Outcome = Retiradas
+     */
+    const { title, value, type } = request.body;
+
+    const transaction = { id: uuid(), title, value, type };
+
+    const income = transactions
+      .filter(t => t.type === 'income')
+      .reduce((inc, t) => inc + t.value, 0);
+
+    const outcome = transactions
+      .filter(t => t.type === 'outcome')
+      .reduce((out, t) => out + t.value, 0);
+
+    const total = income - outcome;
+
+    if (transaction.type === 'outcome' && total - transaction.value < 0) {
+      return response.status(400).json({ error: 'Insufficient Funds' });
+    }
+
+    transactions.push(transaction);
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
